6175acd9838427c4fa71ad682ff9eaf63e4999ee
endurancist <endurancist@gmail.com>
2019-01-18 11:25:12 -0500

    Added node modules

--- graphing.js
+++ graphing.js
@@ -1,83 +1,76 @@
+var hot;
+
 function line() {
 
-    var headers = ['x', 'y'];
+    let headers = ['x', 'y'];
 
-    var myData = [
-        [5.51, 25.72],
-        [7.93, 56.47],
-        [10.02, 101.53],
-        [11.59, 136.88],
-        [15.53, 136.09],
+    let tableData = [
+        {x: 0, y: 25},
+        {x: 1, y: 16},
+        {x: 2, y: 9},
+        {x: 3, y: 4},
+        {x: 4, y: 1},
+        {x: 5, y: 4},
+        {x: 6, y: 9},
+        {x: 7, y: 16},
+        {x: 8, y: 25},
+        {x: 9, y: 36},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
+        {x: '', y: ''},
     ];
 
-    console.log('rendering the spreadsheet');
+    let chartData = [];
 
-    var container = document.getElementById('input-table');
-    var hot = new Handsontable(container, {
-        data: myData,
+    let container = document.getElementById('input-table');
+    hot = new Handsontable(container, {
+        data: tableData,
         rowHeaders: true,
         colHeaders: headers,
-        filters: true,
-        dropdownMenu: true,
+        maxCols: 2,
+        height: 400,
+        width: '100%',
         contextMenu: ['undo', 'redo', '---------', 'row_above', 'row_below', '---------', 'remove_row'],
+        columns: [
+            {data: 'x', type: 'numeric'},
+            {data: 'y', type: 'numeric'},
+        ],
         fillHandle: {
             autoInsertRow: true,
         }
     });
 
-    let ctx = document.getElementById("myChart").getContext('2d');
-    let myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [20, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
+    hot.updateSettings({
+        afterChange: function() {
+            updateChart(chartData, tableData, myChart);
         }
     });
-    console.log('doing chart');
-}
 
-function moon() {
     let ctx = document.getElementById("myChart").getContext('2d');
     let myChart = new Chart(ctx, {
         type: 'line',
         data: {
             datasets: [{
-                label: '# of Votes',
-                data: [
-                    {x: 1.15, y: 12.95},
-                    {x: 3.0566, y: 4.27},
-                    {x: 13.175, y: 20.06},
-                ],
-                borderColor: 'rgba(255,99,132,1)',
+                label: 'y value',
+                data: chartData,
+                borderColor: 'rgba(20, 20, 20, 1)',
                 borderWidth: 2,
                 lineTension: 0.1,
                 fill: false,
@@ -97,4 +90,96 @@ function moon() {
             }
         }
     });
+
+    updateChart(chartData, tableData, myChart);
+
+    // Enabling download function
+    // let canvas = document.getElementById('myChart');
+
+
+}
+
+function moon() {
+    let
+        example = document.getElementById('input-table'),
+        hot;
+
+    let headers = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange", "Pink", 'Brown'];
+    let myData = [
+        [5, 10, 3, 5, 2, 3, 5, 1],
+        [9, 9, 3, 10, 8, 7, 7, 2],
+        [5, 1, 10, 6, 7, 9, 4, 8]
+    ];
+    let colheaders = ['Mark', 'Anna', 'Diana'];
+
+    hot = new Handsontable(example, {
+        data: myData,
+        rowHeaders: colheaders,
+        colHeaders: headers,
+        colWidths: 88,
+        maxCols: 10,
+        fillHandle: {
+            autoInsertRow: false,
+        }
+    });
+
+    hot.updateSettings({
+        afterChange: function() {
+            myChart.update()
+        }
+    });
+
+
+    let options = {
+        type: 'bar',
+        data: {
+            labels: headers,
+            datasets: [{
+                label: colheaders[0],
+                data: myData[0],
+                borderWidth: 1,
+                backgroundColor: 'rgb(255, 236, 217)'
+            }, {
+                label: colheaders[1],
+                data: myData[1],
+                borderWidth: 1,
+                backgroundColor: 'rgb(235, 224, 255)'
+            }, {
+                label: colheaders[2],
+                data: myData[2],
+                borderWidth: 1,
+                backgroundColor: 'rgb(219, 242, 242)'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        reverse: false
+                    }
+                }]
+            }
+        }
+    };
+
+    let ctx = document.getElementById("myChart").getContext('2d');
+    let myChart = new Chart(ctx, options);
+}
+
+function updateChart(chart, table, myChart) {
+    let start = 0;
+    for (let i = 0; i < table.length; i++) {
+        if (table[i]['x'] === "" || table[i]['y'] === "") {
+            continue;
+        }
+        console.log('reach, start = ' + start);
+        chart[start] = {x: table[i]['x'], y: table[i]['y']};
+        start += 1;
+    }
+    console.log('length = ' + chart.length + ', start = ' + start);
+    while (chart.length !== start) {
+        chart.pop();
+    }
+    console.log(chart);
+    myChart.update(0);
 }
