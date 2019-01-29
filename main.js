e7dc6c4de538c5a6666df6966d79abc43e92eba0
endurancist <endurancist@gmail.com>
2019-01-29 12:01:25 -0500

    implemented dual

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ function chartType(chart) {
     } else if (chart === "venus") {
         objects = venus();
     } else {
-        objects = double();
+        objects = dual();
     }
 
     let addRow = document.getElementById('add-row-button');
@@ -105,8 +105,8 @@ function line() {
         colHeaders: ['x', 'y'],
         maxCols: 2,
         columns: [
-            {data: 'x', type: 'numeric'},
-            {data: 'y', type: 'numeric'},
+            {data: 'x', type: 'numeric', numericFormat: {pattern: {mantissa: 2}}},
+            {data: 'y', type: 'numeric', numericFormat: {pattern: {mantissa: 2}}},
         ],
     }));
 
@@ -118,10 +118,10 @@ function line() {
                 label: 'y value',
                 data: chartData,
                 borderColor: rgbString(colors['blue']),
+                backgroundColor: rgbString(colors['white'], 0),
                 borderWidth: 2,
                 lineTension: 0.1,
                 fill: false,
-                backgroundColor: rgbString(colors['white'], 1),
             }]
         },
         options: {
@@ -183,13 +183,14 @@ function scatter() {
                     data: chartData,
                     backgroundColor: rgbString(colors['orange']),
                     pointRadius: 6,
+                    pointHoverRadius: 8,
                     pointBorderWidth: 2,
-                },
-                {
+                }, {
                     label: 'Sun',
                     data: [{x: 0, y: 0}],
                     backgroundColor: rgbString(colors['bright']),
                     pointRadius: 10,
+                    pointHoverRadius: 12,
                     pointBorderWidth: 2,
                 },
             ],
@@ -219,13 +220,91 @@ function venus() {
 
 }
 
-function double() {
+function dual() {
+    let tableData = [
+        {x1: 0, y1: 25, x2: 1, y2: Math.sqrt(100)},
+        {x1: 1, y1: 16, x2: 2, y2: Math.sqrt(200)},
+        {x1: 2, y1: 9, x2: 3, y2: Math.sqrt(300)},
+        {x1: 3, y1: 4, x2: 4, y2: Math.sqrt(400)},
+        {x1: 4, y1: 1, x2: 5, y2: Math.sqrt(500)},
+        {x1: 5, y1: 4, x2: 6, y2: Math.sqrt(600)},
+        {x1: 6, y1: 9, x2: 7, y2: Math.sqrt(700)},
+        {x1: 7, y1: 16, x2: 8, y2: Math.sqrt(800)},
+        {x1: 8, y1: 25, x2: 9, y2: Math.sqrt(900)},
+        {x1: 9, y1: 36, x2: 10, y2: Math.sqrt(1000)},
+        {x1: '', y1: '', x2: 11, y2: Math.sqrt(1100)},
+        {x1: '', y1: '', x2: 12, y2: Math.sqrt(1200)},
+        {x1: '', y1: '', x2: 13, y2: Math.sqrt(1300)},
+        {x1: '', y1: '', x2: 14, y2: Math.sqrt(1400)},
+        {x1: '', y1: '', x2: 15, y2: Math.sqrt(1500)},
+    ];
 
+    let chartData1 = [];
+    let chartData2 = [];
+
+    let container = document.getElementById('table-div');
+    let hot = new Handsontable(container, Object.assign({}, tableCommonOptions, {
+        data: tableData,
+        colHeaders: ['x1', 'y1', 'x2', 'y2'],
+        maxCols: 4,
+        columns: [
+            {data: 'x1', type: 'numeric', numericFormat: {pattern: {mantissa: 2}}},
+            {data: 'y1', type: 'numeric', numericFormat: {pattern: {mantissa: 2}}},
+            {data: 'x2', type: 'numeric', numericFormat: {pattern: {mantissa: 2}}},
+            {data: 'y2', type: 'numeric', numericFormat: {pattern: {mantissa: 2}}},
+        ],
+    }));
+
+    let ctx = document.getElementById("myChart").getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [
+                {
+                    label: 'y1',
+                    data: chartData1,
+                    borderColor: rgbString(colors['blue']),
+                    backgroundColor: rgbString(colors['white'], 0),
+                    borderWidth: 2,
+                    lineTension: 0.1,
+                    fill: false,
+                }, {
+                    label: 'y2',
+                    data: chartData2,
+                    borderColor: rgbString(colors['red']),
+                    backgroundColor: rgbString(colors['white'], 0),
+                    borderWidth: 2,
+                    lineTension: 0.1,
+                    fill: false,
+                }
+            ]
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom',
+                }],
+            }
+        }
+    });
+
+    hot.updateSettings({
+        afterChange: function() {
+            updateDual(tableData, myChart, 0);
+            updateDual(tableData, myChart, 1);
+        }
+    });
+
+    updateDual(tableData, myChart, 0);
+    updateDual(tableData, myChart, 1);
+
+    return [hot, myChart];
 }
 
-function updateLine(table, myChart) {
+function updateLine(table, myChart, dataSet = 0) {
     let start = 0;
-    let chart = myChart.data.datasets[0].data;
+    let chart = myChart.data.datasets[dataSet].data;
     for (let i = 0; i < table.length; i++) {
         if (table[i]['x'] === '' || table[i]['y'] === '') {
             continue;
@@ -252,6 +331,23 @@ function updateScatter(table, myChart) {
             x: Math.cos(la / 180 * Math.PI) * di * Math.cos(lo / 180 * Math.PI),
             y: Math.cos(la / 180 * Math.PI) * di * Math.sin(lo / 180 * Math.PI),
         }
+    }
+    while (chart.length !== start) {
+        chart.pop();
+    }
+    myChart.update(0);
+}
+
+function updateDual(table, myChart, dataSet = 0) {
+    let start = 0;
+    let chart = myChart.data.datasets[dataSet].data;
+    for (let i = 0; i < table.length; i++) {
+        let xKey = 'x' + (dataSet + 1);
+        let yKey = 'y' + (dataSet + 1);
+        if (table[i][xKey] === '' || table[i][yKey] === '') {
+            continue;
+        }
+        chart[start++] = {x: table[i][xKey], y: table[i][yKey]};
     }
     while (chart.length !== start) {
         chart.pop();
