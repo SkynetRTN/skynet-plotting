c371d3f9521dc864f7e68fc6e66ba03d6424c42a
endurancist <endurancist@gmail.com>
2019-03-13 19:57:39 -0700

    rearranged some functions so that helper functions are in the bottom now

--- main.js
+++ main.js
@@ -1131,69 +1131,6 @@ function updateScatter(table, myChart) {
 }
 
 /**
- * This function takes an angle in degrees and returns it in radians.
- * @param degree:   An angle in degrees
- * @returns {number}
- */
-function rad(degree) {
-    return degree / 180 * Math.PI;
-}
-
-/**
- * This function returns the square of the input n.
- * @param n:        The input number to be squared.
- * @returns {number}
- */
-function sqr(n) {
-    return Math.pow(n, 2);
-}
-
-/**
- * This function takes a floating point number and round it to a specified decimal places.
- * @param value:    The value to be rounded.
- * @param digits:   The decimal places to round the value.
- * @returns {number}
- */
-function round(value, digits) {
-    return Math.round(value * Math.pow(10, digits)) / Math.pow(10, digits);
-}
-
-/**
- * This function takes a string containing a hexadecimal number and return a rgb string represented
- * by the value of rgb.
- * @param rgb:      A string containing a hexadecimal number which represents a rgb value.
- * @param opacity:  Opacity of the returned rgb string. Default is 1.
- * @returns {string}
- */
-function rgbString(rgb, opacity=1) {
-    let r = hexToDecimal(rgb, 1, 2);
-    let g = hexToDecimal(rgb, 3, 4);
-    let b = hexToDecimal(rgb, 5, 6);
-    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + opacity + ')';
-}
-
-/**
- * This function takes a portion of string which contains a hexadecimal number and returns a decimal
- * number of the same value with the hex number.
- * @param hex:      The whole string.
- * @param s:        The starting position of the portion of the string. Inclusive.
- * @param t:        The ending position of the portion of the string. Inclusive.
- * @returns {number}
- */
-function hexToDecimal(hex, s, t) {
-    let result = 0;
-    for (let i = s; i <= t; i++) {
-        result <<= 4;
-        if (hex[i] >= '0' && hex[i] <='9') {
-            result += hex[i].charCodeAt(0) - '0'.charCodeAt(0);
-        } else {
-            result += hex[i].charCodeAt(0) - 'a'.charCodeAt(0) + 10;
-        }
-    }
-    return result;
-}
-
-/**
  * This function takes a Chartjs object and a form containing information (Title, data labels, X axis label,
  * Y axis label) about the chart, and updates corresponding properties of the chart.
  * @param myChart:  The Chartjs object to be updated.
@@ -1259,4 +1196,67 @@ function removeSpaces(str) {
     } else {
         return str.substr(start, end - start + 1);
     }
+}
+
+/**
+ * This function takes a string containing a hexadecimal number and return a rgb string represented
+ * by the value of rgb.
+ * @param rgb:      A string containing a hexadecimal number which represents a rgb value.
+ * @param opacity:  Opacity of the returned rgb string. Default is 1.
+ * @returns {string}
+ */
+function rgbString(rgb, opacity=1) {
+    let r = hexToDecimal(rgb, 1, 2);
+    let g = hexToDecimal(rgb, 3, 4);
+    let b = hexToDecimal(rgb, 5, 6);
+    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + opacity + ')';
+}
+
+/**
+ * This function takes a portion of string which contains a hexadecimal number and returns a decimal
+ * number of the same value with the hex number.
+ * @param hex:      The whole string.
+ * @param s:        The starting position of the portion of the string. Inclusive.
+ * @param t:        The ending position of the portion of the string. Inclusive.
+ * @returns {number}
+ */
+function hexToDecimal(hex, s, t) {
+    let result = 0;
+    for (let i = s; i <= t; i++) {
+        result <<= 4;
+        if (hex[i] >= '0' && hex[i] <='9') {
+            result += hex[i].charCodeAt(0) - '0'.charCodeAt(0);
+        } else {
+            result += hex[i].charCodeAt(0) - 'a'.charCodeAt(0) + 10;
+        }
+    }
+    return result;
+}
+
+/**
+ * This function takes an angle in degrees and returns it in radians.
+ * @param degree:   An angle in degrees
+ * @returns {number}
+ */
+function rad(degree) {
+    return degree / 180 * Math.PI;
+}
+
+/**
+ * This function returns the square of the input n.
+ * @param n:        The input number to be squared.
+ * @returns {number}
+ */
+function sqr(n) {
+    return Math.pow(n, 2);
+}
+
+/**
+ * This function takes a floating point number and round it to a specified decimal places.
+ * @param value:    The value to be rounded.
+ * @param digits:   The decimal places to round the value.
+ * @returns {number}
+ */
+function round(value, digits) {
+    return Math.round(value * Math.pow(10, digits)) / Math.pow(10, digits);
 }
