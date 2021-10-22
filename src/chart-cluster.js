aeb0d151565cc466f63dcca825508514a5032666
John Torian <johntorian@outlook.com>
2021-10-22 19:49:12 -0400

    Rearranged formatting. THE NAMES OF THE COLOR FILTERS HAVE CHANGED

--- src/chart-cluster.js
+++ src/chart-cluster.js
@@ -12,40 +12,45 @@ export function cluster() {
     document.getElementById('input-div').insertAdjacentHTML('beforeend',
         '<form title="Cluster Diagram" id="cluster-form">\n' +
         '<div class="row">\n' +
-        '<div class="col-sm-3 des">Distance (kpc)</div>\n' +
-        '<div class="col-sm-6 range"><input type="range" title="d" name="d"></div>\n' +
+        '<div class="col-sm-4 des">Dist. (kpc)</div>\n' +
+        '<div class="col-sm-5 range"><input type="range" title="d" name="d"></div>\n' +
         '<div class="col-sm-3 text"><input type="number" title="d" name="d-num" class="field"></div>\n' +
         '</div>\n' +
         '<div class="row">\n' +
-        '<div class="col-sm-3 des">+/- Range (%)</div>\n' +
-        '<div class="col-sm-6 range"><input type="range" title="R" name="r"></div>\n' +
+        '<div class="col-sm-4 des">+/- Range (%)</div>\n' +
+        '<div class="col-sm-5 range"><input type="range" title="R" name="r"></div>\n' +
         '<div class="col-sm-3 text"><input type="number" title="R" name="r-num" class="field"></div>\n' +
         '</div>\n' +
         '<div class="row">\n' +
-        '<div class="col-sm-3 des">Age (yr)</div>\n' +
-        '<div class="col-sm-6 range"><input type="range" title="Age" name="age"></div>\n' +
+        '<div class="col-sm-4 des">Age (yr)</div>\n' +
+        '<div class="col-sm-5 range"><input type="range" title="Age" name="age"></div>\n' +
         '<div class="col-sm-3 text"><input type="number" title="Age" name="age-num" class="field"></div>\n' +
         '</div>\n' +
         '<div class="row">\n' +
-        '<div class="col-sm-3 des">Reddening (mag)</div>\n' +
-        '<div class="col-sm-6 range"><input type="range" title="Reddening" name="red"></div>\n' +
+        '<div class="col-sm-4 des">Reddening (mag)</div>\n' +
+        '<div class="col-sm-5 range"><input type="range" title="Reddening" name="red"></div>\n' +
         '<div class="col-sm-3 text"><input type="number" title="Reddening" name="red-num" class="field"></div>\n' +
         '</div>\n' +
         '<div class="row">\n' +
-        '<div class="col-sm-3 des">Metallicity (solar)</div>\n' +
-        '<div class="col-sm-6 range"><input type="range" title="Metallicity" name="metal"></div>\n' +
+        '<div class="col-sm-4 des">Metallicity (solar)</div>\n' +
+        '<div class="col-sm-5 range"><input type="range" title="Metallicity" name="metal"></div>\n' +
         '<div class="col-sm-3 text"><input type="number" title="Metallicity" name="metal-num" class="field"></div>\n' +
         '</div>\n' +
         '<div class="row">\n' +
         '<div class="col-sm-6"><b>Select Filters:</b></div>\n'+
         '</div>\n'+
         '<div class="row">\n' +
-        '<div class="col-sm-6">Blue</div>\n'+
-        '<div class="col-sm-6">Luminosity</div>\n'+
+        '<div class="col-sm-4">Blue Color</div>\n'+
+        '<div class="col-sm-4">Red Color</div>\n'+
+        '<div class="col-sm-4">Luminosity</div>\n'+
         '</div>\n' +
         '<div class="row">\n' +
-        '<div class="col-sm-6"><select name="Blue Color Filter" style="width: 100%;" title="Select Blue Color Filter"></select></div>\n'+
-        '<div class="col-sm-6"><select name="Luminosity Filter" style="width: 100%;" title="Select Luminosity Filter"></select></div>\n'+
+        '<div class="col-sm-4"><select name="blue-color-filter" style="width: 100%;" title="Select Blue Color Filter">\n'+
+        '<option value="B" title="B filter" selected>B</option></select></div>\n'+
+        '<div class="col-sm-4"><select name="red-color-filter" style="width: 100%;" title="Red Color Filter" disabled>\n'+
+        '<option value="V" title="V filter" selected>V</option></select></div>\n'+
+        '<div class="col-sm-4"><select name="luminosity-filter" style="width: 100%;" title="Select Luminosity Filter">\n'+
+        '<option value="V" title="V filter" selected>Lum</option></select></div>\n'+
         '</div>\n' +
         '</form>\n');
 
