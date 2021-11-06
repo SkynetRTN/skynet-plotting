dc1fdecbb4cf7011c1b1305feead68533103edbc
shengjiex98 <shengjiex98@gmail.com>
2021-11-06 19:31:51 -0400

    Added development server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
