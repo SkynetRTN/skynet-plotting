8acb8176804c6fce0ca9a959a5b450a28bdc754a
shengjiex98 <shengjiex98@gmail.com>
2022-01-02 02:40:26 -0500

    Venus is TS!

--- src/chart-venus.ts
+++ src/chart-venus.ts
@@ -2,16 +2,17 @@
 
 import Chart from "chart.js/auto";
 import Handsontable from "handsontable";
+import { ChartConfiguration, ScatterDataPoint } from "chart.js";
 
 import { tableCommonOptions, colors } from "./config"
 import { updateLine, updateLabels, updateTableHeight } from "./util"
-import { round, sqr, rad } from "./my-math"
+import { sqr, rad } from "./my-math"
 
 /**
  *  Function for comparing data points with both Heliocentric and geocentric models.
  *  @returns {[Handsontable, Chart]}
  */
-export function venus() {
+export function venus(): [Handsontable, Chart] {
     /**
      *  The following lines are used for exploring the effect of changing x have in geocentric model.
      *  The final value selected for rendering the chart is x = 0.445 (upper) and x = 0.8 (lower).
@@ -54,11 +55,9 @@ export function venus() {
         { x: '', y: '' },
     ];
 
-    let chartData = [];
-
     // create table
     const container = document.getElementById('table-div');
-    const hot = new Handsontable(container, Object.assign({}, tableCommonOptions, {
+    const tableOptions: Handsontable.GridSettings = {
         data: tableData,
         colHeaders: ['Angular Diameter', 'Phase of Venus'],
         maxCols: 2,
@@ -66,17 +65,18 @@ export function venus() {
             { data: 'x', type: 'numeric', numericFormat: { pattern: { mantissa: 2 } } },
             { data: 'y', type: 'numeric', numericFormat: { pattern: { mantissa: 2 } } },
         ],
-    }));
+    };
+    const hot = new Handsontable(container, {...tableCommonOptions, ...tableOptions});
 
     // create chart
-    const ctx = document.getElementById("myChart").getContext('2d');
-    const myChart = new Chart(ctx, {
+    const ctx = (document.getElementById("myChart") as HTMLCanvasElement).getContext('2d');
+    const chartOptions: ChartConfiguration = {
         type: 'line',
         data: {
             datasets: [
                 {
                     label: 'Data',
-                    data: chartData,
+                    data: [],
                     backgroundColor: colors['orange'],
                     fill: false,
                     showLine: false,
@@ -134,14 +134,11 @@ export function venus() {
                     position: 'bottom',
                     suggestedMin: 5,
                     suggestedMax: 65,
-                },
-                y: {
-                    // stacked: true,
-                    // suggestedMin: -2,
                 }
             }
         }
-    });
+    };
+    const myChart = new Chart(ctx, chartOptions) as Chart<'line'>;
 
     const update = function () {
         updateLine(tableData, myChart);
@@ -160,23 +157,19 @@ export function venus() {
     myChart.options.plugins.title.text = "Title";
     myChart.options.scales['x'].title.text = "x";
     myChart.options.scales['y'].title.text = "y";
-    updateLabels(myChart, document.getElementById('chart-info-form'));
+    updateLabels(myChart, document.getElementById('chart-info-form') as ChartInfoForm);
 
     return [hot, myChart];
 }
 
 // Distance from Sun to Earth in km
 const dE = 1.496e8;
-
 // Distance from Sun to Venus in km
 const dV = 1.082e8;
-
 // Diameter of Venus in km
 const DV = 1.210e4;
-
 // Max angular separation between Venus and Sun in radians.
 const beta = rad(45);
-
 // Angular diameter of Venus as its closest in arc-seconds.
 const maxA = 60;
 
@@ -189,21 +182,23 @@ const maxA = 60;
 *  @param steps:   The number of data points to be generated. Default is 500.
 *  @returns {Array}
 */
-function geocentric(start, end, x, steps = 500) {
-    let data = [];
+function geocentric(start:number, end:number, x:number, steps: number = 500): ScatterDataPoint[] {
+    const data: ScatterDataPoint[] = [];
+    const step = (end - start) / steps;
+
     let a = start;
-    let step = (end - start) / steps;
     for (let i = 0; i < steps; i++) {
-        let d = (1 - x) * (1 - Math.sin(beta)) * maxA * dE / a;
+        const d: number = (1 - x) * (1 - Math.sin(beta)) * maxA * dE / a;
 
         // In geocentric model dV is a variable, so we need to override it
-        let dV = Math.sqrt((1 - x) * sqr(Math.sin(beta)) * sqr(dE) + x * sqr(dE) - x / (1 - x) * sqr(d));
-
-        let cosPhi = (sqr(d) + sqr(dV) - sqr(dE)) / (2 * d * dV);
+        const dV: number = Math.sqrt((1 - x) * sqr(Math.sin(beta)) * sqr(dE) + x * sqr(dE) - x / (1 - x) * sqr(d));
+        const cosPhi: number = (sqr(d) + sqr(dV) - sqr(dE)) / (2 * d * dV);
 
         data.push({
             x: a,
-            y: (1 + cosPhi) / 2 > 0 ? (1 + cosPhi) / 2 : '',
+            y: (1 + cosPhi) / 2 > 0 ? (1 + cosPhi) / 2 : 0,
+            // How is below different from above????
+            // y: Math.max((1 + cosPhi) / 2, 0) // this doesn't work. I'm so confused
         });
         a += step;
     }
@@ -217,13 +212,14 @@ function geocentric(start, end, x, steps = 500) {
 *  @param steps:   The number of data points to be generated. Default is 500.
 *  @returns {Array}
 */
-function heliocentric(start, end, steps = 500) {
-    let data = [];
+function heliocentric(start: number, end: number, steps: number = 500): ScatterDataPoint[] {
+    const data: ScatterDataPoint[] = [];
+    const step = (end - start) / steps;
+
     let a = start;
-    let step = (end - start) / steps;
     for (let i = 0; i < steps; i++) {
-        let theta = Math.acos((sqr(DV) / sqr(rad(a / 3600)) - (sqr(dE) + sqr(dV))) / (2 * dE * dV));
-        let alpha = Math.atan(dV * Math.sin(theta) / (dE + dV * Math.cos(theta)));
+        const theta = Math.acos((sqr(DV) / sqr(rad(a / 3600)) - (sqr(dE) + sqr(dV))) / (2 * dE * dV));
+        const alpha = Math.atan(dV * Math.sin(theta) / (dE + dV * Math.cos(theta)));
         data.push({
             x: a,
             y: (1 - Math.cos(Math.PI - theta + alpha)) / 2,
