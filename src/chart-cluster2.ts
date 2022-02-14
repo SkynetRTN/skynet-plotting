007daef1016ba96413b433eca6eb58ab27fdbb78
Logggy <selphlogan75@gmail.com>
2022-02-13 22:43:16 -0500

    Fixed title stuff cluster 2

--- src/chart-cluster2.ts
+++ src/chart-cluster2.ts
@@ -393,9 +393,14 @@ export function cluster2(): [Handsontable, Chart, Chart, ModelForm] {
         },
         title: {
           display: true,
-          align: "start",
+          align: "end",
+          padding: {
+            top: 45,
+            bottom: 5,
+          }
         },
         legend: {
+          display: false,
           align: "end",
           labels: {
             filter: function(item) {
@@ -497,7 +502,7 @@ console.log('ayyo2');
           },
         },
         legend: {
-          align: "end",
+          align: "start",
           labels: {
             filter: function(item) {
               // Logic to remove a particular legend item goes here
