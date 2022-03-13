0d046d2ff742eae92d2a47d608d6214dc8107ab6
Loggy <you@example.com>
2022-03-13 16:05:28 -0400

    fix

--- src/chart-cluster-utils/chart-gaia-util.ts
+++ src/chart-cluster-utils/chart-gaia-util.ts
@@ -48,5 +48,4 @@ export function sortStarDuplicates(dataArray: starData[]): starData[]{
     return uniqueStars
 }
 //when the database returns the star ranges and id's, export a function that matches them back to the magnitude and filter data
-export function matchStarData(stars: starData[], data: Map<string, Map<string, number>>){ 
-    
+//export function matchStarData(stars: starData[], data: Map<string, Map<string, number>>){ 
