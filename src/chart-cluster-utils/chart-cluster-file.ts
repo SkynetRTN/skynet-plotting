8bbb566f08fa77f26fedc4faaf021d5208eab616
Reed Fu <reed.fu@unc.edu>
2022-03-15 00:44:25 -0400

    implementing requesting gaia data and store in table

--- src/chart-cluster-utils/chart-cluster-file.ts
+++ src/chart-cluster-utils/chart-cluster-file.ts
@@ -5,10 +5,11 @@
 
 import { Chart } from "chart.js";
 import Handsontable from "handsontable";
-import { modelFormKey } from "./chart-cluster-util";
+import {httpPostAsync, modelFormKey } from "./chart-cluster-util";
 import {changeOptions, updateLabels, updateTableHeight } from "../util";
 import { updateHRModel } from "./chart-cluster-model";
 import { graphScale, updateScatter } from "./chart-cluster-scatter";
+import {starData, sortStar} from "./chart-cluster-gaia";
 
 /**
  * This function handles the uploaded file to the variable chart. Specifically, it parse the file
@@ -80,180 +81,201 @@ export function clusterFileUpload(
         //store these in the table
 
         //request server to get the data
-        // put everythin in callback
-        //fills the dictionary datadict with objects for each source, having attributes of each filter magnitude
-        for (const row of data) {
+        let stars: starData[] = []
+        for (let row of data) {
             let items = row.trim().split(",");
             let src = items[keyIndex['id']]
-            if (!datadict.has(src)) {
-                datadict.set(src, new Map<string, number>());
-            }
             let filter = items[keyIndex['filter']];
             let calibratedMag = parseFloat(items[keyIndex['calibrated_mag\\r']]);
-            let err = parseFloat(items[keyIndex['mag_error']]);
-            let ra = parseFloat(items[keyIndex['ra_hours']])*24;
+            let ra = parseFloat(items[keyIndex['ra_hours']])*15;
             let dec = parseFloat(items[keyIndex['dec_degs']]);
-            if (items[keyIndex['mag']] !== "") {
-                datadict.get(src).set(filter, isNaN(calibratedMag) ? null : calibratedMag);
-                datadict.get(src).set(filter + "err", isNaN(err) ? 0 : err);
-                datadict.get(src).set(filter + "ra", isNaN(ra) ? null : ra);
-                datadict.get(src).set(filter + "dec", isNaN(dec) ? null : dec);
-                datadict.get(src).set(filter + "dist", null)
-                datadict.get(src).set(filter + "pmra", null)
-                datadict.get(src).set(filter + "pmdec", null)
-                if (!filters.includes(filter)) {
-                    filters.push(filter);
+            let mag = items[keyIndex['mag']];
+            let err = parseFloat(items[keyIndex['mag_error']]);
+            stars.push(new starData(src, filter, err, calibratedMag, mag, ra, dec, null, [null, null]))
+        }
+        let cleanedup = sortStar(stars)
+
+        let url: string = "http://localhost:5000/gaia"
+        httpPostAsync(url, cleanedup[1], (result: string)=>{
+            // console.log(JSON.parse(result))
+
+            let gaia = JSON.parse(result)
+            let sortedData = cleanedup[0]
+            let gaia_i = 0;
+            let data_i = 0;
+            while (true) {
+                try {
+                    // @ts-ignore
+                    if (sortedData[data_i]['id'] === gaia[gaia_i]['id']) {
+                        // @ts-ignore
+                        sortedData[data_i]['motion'] = gaia[gaia_i]['pm']
+                        let star = sortedData[data_i]
+                        let src = star['id']
+                        let filter = star['filter']
+                        if (!datadict.has(src)) {
+                            datadict.set(src, new Map<string, number>());
+                        }
+                        if (star['mag'] !== "") {
+                            datadict.get(src).set(filter, isNaN(star['filteredMag']) ? null : star['filteredMag']);
+                            datadict.get(src).set(filter + "err", isNaN(star['err']) ? 0 : star['err']);
+                            datadict.get(src).set(filter + "ra", isNaN(star['ra']) ? null : star['ra']);
+                            datadict.get(src).set(filter + "dec", isNaN(star['dec']) ? null : star['dec']);
+                            datadict.get(src).set(filter + "dist", null === (star['distance']) ? null : star['distance'])
+                            datadict.get(src).set(filter + "pmra", null === (star['motion'][0]) ? null : star['motion'][0])
+                            datadict.get(src).set(filter + "pmdec", null === (star['motion'][1]) ? null : star['motion'][1])
+                            if (!filters.includes(filter)) {
+                                filters.push(filter);
+                            }
+                        }
+                        data_i++;
+                    } else {
+                        gaia_i++;
+                    }
+                } catch(err) {
+                    break
                 }
             }
-        }
 
-        //add null values for sources that didn't show up under each filter
-        for (const src of datadict.keys()) {
-            for (const f of filters) {
-                if (!datadict.get(src).has(f)) {
-                    datadict.get(src).set(f, null);
-                    datadict.get(src).set(f + "err", null);
-                    datadict.get(src).set(f + "ra", null);
-                    datadict.get(src).set(f + "dec", null);
-                    datadict.get(src).set(f + "dist", null)
-                    datadict.get(src).set(f + "pmra", null)
-                    datadict.get(src).set(f + "pmdec", null)
+            //add null values for sources that didn't show up under each filter
+            for (const src of datadict.keys()) {
+                for (const f of filters) {
+                    if (!datadict.get(src).has(f)) {
+                        datadict.get(src).set(f, null);
+                        datadict.get(src).set(f + "err", null);
+                        datadict.get(src).set(f + "ra", null);
+                        datadict.get(src).set(f + "dec", null);
+                        datadict.get(src).set(f + "dist", null)
+                        datadict.get(src).set(f + "pmra", null)
+                        datadict.get(src).set(f + "pmdec", null)
+                    }
                 }
             }
-        }
 
-        //sortStarDuplicates(stars);
-        //maxMinRaDec(stars);
+            let blue = modelForm[modelFormKey(0, 'blue')];
+            let red = modelForm[modelFormKey(0, 'red')];
+            let lum = modelForm[modelFormKey(0, 'lum')];
 
-        //console.log(sortStarDuplicates(stars))
-        //console.log(maxMinRaDec(stars))
+            //order filters by temperature
+            const knownFilters = ["U", "uprime", "B", "gprime", "V", "rprime", "R", "iprime", "I", "zprime", "Y", "J", "H", "Ks", "K",];
+            //knownFilters is ordered by temperature; this cuts filters not in the file from knownFilters, leaving the filters in the file in order.
+            filters = knownFilters.filter((f) => filters.indexOf(f) >= 0);
+            //if it ain't known ignore it
 
-        let blue = modelForm[modelFormKey(0, 'blue')];
-        let red = modelForm[modelFormKey(0, 'red')];
-        let lum = modelForm[modelFormKey(0, 'lum')];
-
-        //order filters by temperature
-        const knownFilters = ["U", "uprime", "B", "gprime", "V", "rprime", "R", "iprime", "I", "zprime", "Y", "J", "H", "Ks", "K",];
-        //knownFilters is ordered by temperature; this cuts filters not in the file from knownFilters, leaving the filters in the file in order.
-        filters = knownFilters.filter((f) => filters.indexOf(f) >= 0);
-        //if it ain't known ignore it
-
-        const optionList = [];
-        const headers: any[] = [];
-        const columns: any[] = [];
-        let hiddenColumns: any[] = [];
-        for (let i = 0; i < filters.length; i++) {
-            //makes a list of options for each filter
-            optionList.push({
-                value: filters[i],
-                title: filters[i] + " Mag",
-                text: filters[i],
-            });
-            for (let j = 0; j < 7; j++) {
-                //we have to multiply up the length for the error and gaia data
-                hiddenColumns[i + j * filters.length] = i + j * filters.length;
-            }
-            headers.push(filters[i] + " Mag"); //every other column is err
-            headers.push(filters[i] + " err");
-            headers.push(filters[i] + " ra");
-            headers.push(filters[i] + " dec");
-            headers.push(filters[i] + " dist");
-            headers.push(filters[i] + " pmra");
-            headers.push(filters[i] + " pmdec");
-            columns.push({
-                data: filters[i],
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-            columns.push({
-                data: filters[i] + "err",
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-            columns.push({
-                data: filters[i] + "ra",
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-            columns.push({
-                data: filters[i] + "dec",
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-            columns.push({
-                data: filters[i] + "dist",
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-            columns.push({
-                data: filters[i] + "pmra",
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-            columns.push({
-                data: filters[i] + "pmdec",
-                type: "numeric",
-                numericFormat: {pattern: {mantissa: 2}},
-            });
-        }
-        console.log(hiddenColumns)
-        hiddenColumns = hiddenColumns.filter((c) => [0, 7].indexOf(c) < 0); //get rid of the columns we want revealed
-        console.log(headers)
-        console.log(hiddenColumns)
-        //convrt datadict from dictionary to nested number array tableData
-        const tableData: { [key: string]: number }[] = [];
-        datadict.forEach((src) => {
-            const row: { [key: string]: number } = {};
-            for (let filterIndex in filters) {
-                row[filters[filterIndex]] = src.get(filters[filterIndex]);
-                row[filters[filterIndex] + "err"] = src.get(filters[filterIndex] + "err");
-                row[filters[filterIndex] + "ra"] = src.get(filters[filterIndex] + "ra");
-                row[filters[filterIndex] + "dec"] = src.get(filters[filterIndex] + "dec");
-                row[filters[filterIndex] + "dist"] = src.get(filters[filterIndex] + "dist");
-                row[filters[filterIndex] + "pmra"] = src.get(filters[filterIndex] + "pmra");
-                row[filters[filterIndex] + "pmdec"] = src.get(filters[filterIndex] + "pmdec");
-            }
-           
-            tableData.push(row);
-        });
-        for (let c = 0; c < myCharts.length; c++) {
-            let myChart = myCharts[c] as Chart<'line'>;
-            myChart.options.plugins.title.text = "Title";
-            myChart.data.datasets[2].label = "Data";
-            myChart.options.scales['x'].title.text = ('x' + (c+1).toString());
-            myChart.options.scales['y'].title.text = ('y' + (c+1).toString());
-            updateLabels(myChart, document.getElementById("chart-info-form") as ChartInfoForm, false, false, false, false, c);
-
-            //Change filter options to match file
-            blue = modelForm[modelFormKey(c, 'blue')];
-            red = modelForm[modelFormKey(c, 'red')];
-            lum = modelForm[modelFormKey(c, 'lum')];
-
-            //Change the options in the drop downs to the file's filters
-            //blue and lum are most blue by default, red is set to most red
-            changeOptions(blue, optionList);
-            changeOptions(red, optionList);
-            //red.value = red.options[red.options.length-1].value;
-            changeOptions(lum, optionList);
-
-            blue.value = filters[0];
-            red.value = filters[1];
-            lum.value = filters[1];
-
-            updateHRModel(modelForm, table, [myChart],
-                () => {
-                    table.updateSettings({
-                        data: tableData,
-                        colHeaders: headers,
-                        columns: columns,
-                        hiddenColumns: {columns: hiddenColumns},
-                    }); //hide all but the first 3 columns
-                    updateTableHeight(table);
-                    graphMaxMin.updateMode('auto', c);
-                    updateScatter(table, [myChart], clusterForm, modelForm, [2], graphMaxMin);
-                    document.getElementById("standardView").click();
+            const optionList = [];
+            const headers: any[] = [];
+            const columns: any[] = [];
+            let hiddenColumns: any[] = [];
+            for (let i = 0; i < filters.length; i++) {
+                //makes a list of options for each filter
+                optionList.push({
+                    value: filters[i],
+                    title: filters[i] + " Mag",
+                    text: filters[i],
                 });
-        }
+                for (let j = 0; j < 7; j++) {
+                    //we have to multiply up the length for the error and gaia data
+                    hiddenColumns[i + j * filters.length] = i + j * filters.length;
+                }
+                headers.push(filters[i] + " Mag"); //every other column is err
+                headers.push(filters[i] + " err");
+                headers.push(filters[i] + " ra");
+                headers.push(filters[i] + " dec");
+                headers.push(filters[i] + " dist");
+                headers.push(filters[i] + " pmra");
+                headers.push(filters[i] + " pmdec");
+                columns.push({
+                    data: filters[i],
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+                columns.push({
+                    data: filters[i] + "err",
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+                columns.push({
+                    data: filters[i] + "ra",
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+                columns.push({
+                    data: filters[i] + "dec",
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+                columns.push({
+                    data: filters[i] + "dist",
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+                columns.push({
+                    data: filters[i] + "pmra",
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+                columns.push({
+                    data: filters[i] + "pmdec",
+                    type: "numeric",
+                    numericFormat: {pattern: {mantissa: 2}},
+                });
+            }
+            hiddenColumns = hiddenColumns.filter((c) => [0, 7].indexOf(c) < 0); //get rid of the columns we want revealed
+
+            //convrt datadict from dictionary to nested number array tableData
+            const tableData: { [key: string]: number }[] = [];
+            datadict.forEach((src) => {
+                const row: { [key: string]: number } = {};
+                for (let filterIndex in filters) {
+                    row[filters[filterIndex]] = src.get(filters[filterIndex]);
+                    row[filters[filterIndex] + "err"] = src.get(filters[filterIndex] + "err");
+                    row[filters[filterIndex] + "ra"] = src.get(filters[filterIndex] + "ra");
+                    row[filters[filterIndex] + "dec"] = src.get(filters[filterIndex] + "dec");
+                    row[filters[filterIndex] + "dist"] = src.get(filters[filterIndex] + "dist");
+                    row[filters[filterIndex] + "pmra"] = src.get(filters[filterIndex] + "pmra");
+                    row[filters[filterIndex] + "pmdec"] = src.get(filters[filterIndex] + "pmdec");
+                }
+
+                tableData.push(row);
+            });
+            for (let c = 0; c < myCharts.length; c++) {
+                let myChart = myCharts[c] as Chart<'line'>;
+                myChart.options.plugins.title.text = "Title";
+                myChart.data.datasets[2].label = "Data";
+                myChart.options.scales['x'].title.text = ('x' + (c+1).toString());
+                myChart.options.scales['y'].title.text = ('y' + (c+1).toString());
+                updateLabels(myChart, document.getElementById("chart-info-form") as ChartInfoForm, false, false, false, false, c);
+
+                //Change filter options to match file
+                blue = modelForm[modelFormKey(c, 'blue')];
+                red = modelForm[modelFormKey(c, 'red')];
+                lum = modelForm[modelFormKey(c, 'lum')];
+
+                //Change the options in the drop downs to the file's filters
+                //blue and lum are most blue by default, red is set to most red
+                changeOptions(blue, optionList);
+                changeOptions(red, optionList);
+                //red.value = red.options[red.options.length-1].value;
+                changeOptions(lum, optionList);
+
+                blue.value = filters[0];
+                red.value = filters[1];
+                lum.value = filters[1];
+
+                updateHRModel(modelForm, table, [myChart],
+                    () => {
+                        table.updateSettings({
+                            data: tableData,
+                            colHeaders: headers,
+                            columns: columns,
+                            hiddenColumns: {columns: hiddenColumns},
+                        }); //hide all but the first 3 columns
+                        updateTableHeight(table);
+                        graphMaxMin.updateMode('auto', c);
+                        updateScatter(table, [myChart], clusterForm, modelForm, [2], graphMaxMin);
+                        document.getElementById("standardView").click();
+                    });
+            }
+        })
     };
     reader.readAsText(file);
 }
--- src/chart-cluster-utils/chart-cluster-gaia.ts
+++ src/chart-cluster-utils/chart-cluster-gaia.ts
@@ -1,39 +1,45 @@
 export class starData{
     id: string
+
+    filter: string
+    err: number
+    filteredMag: number
+    mag: string
     ra: number
     dec: number
     radius: number
     distance: number
     motion: number []
-    constructor(id: string, ra: number, dec: number, distance: number, motion: number[]){
-        this.id = id
-        this.ra = ra
-        this.dec = dec
-        this.distance = distance
-        this.motion = motion
+    constructor(id: string, filter: string, err: number, filteredMag: number, mag: string, ra: number, dec: number, distance: number, motion: number[]){
+        this.id = id;
+        this.filter = filter;
+        this.err = err
+        this.filteredMag = filteredMag
+        this.mag = mag;
+        this.ra = ra;
+        this.dec = dec;
+        this.distance = distance;
+        this.motion = motion;
     }
-}
-export class gaiaData{
-    id: string
-    distance: number
-    motion: number []
 }
 
-export function sortStar(dataArray: starData[]): starData[][]{
+export function sortStar(dataArray: starData[]): [starData[], any]{
     let sortedStars = mergeSortStar(dataArray);
     //delete the duplicates
-    let uniqueStars = []
-    let lastStar = sortedStars[0]
-    let minMax: number[] = [lastStar.ra, lastStar.ra, lastStar.dec, lastStar.dec]
-    uniqueStars.push(lastStar)
+    let uniqueStars = [];
+    let lastStar = {id: sortedStars[0].id, ra: sortedStars[0].ra, dec: sortedStars[0].dec};
+    let minMax: number[] = [lastStar.ra, lastStar.ra, lastStar.dec, lastStar.dec];
+    uniqueStars.push(lastStar);
     for (let i = 1; i < sortedStars.length; i++){
-        if (sortedStars[i].id !== lastStar.id){
-            lastStar = sortedStars[i]
-            uniqueStars.push(lastStar)
-            minMax = maxMinRaDec(lastStar, minMax)
+        if (sortedStars[i].id !== lastStar['id']){
+            lastStar = {id: sortedStars[i].id, ra: sortedStars[i].ra, dec: sortedStars[i].dec};
+            uniqueStars.push(lastStar);
+            minMax = maxMinRaDec(sortedStars[i], minMax);
         }
     }
-    return [sortedStars, uniqueStars]
+    console.log(minMax)
+    let query = {'data': uniqueStars, 'range': getClusterCenter(minMax, uniqueStars)}
+    return [sortedStars, query]
 }
 
 
@@ -42,47 +48,89 @@ function mergeSortStar(dataArray: starData[]): starData[]{
     //let idArray = starData.map(star => star.id)
     //break the array into chunks of size 2
     //let chunk1 = idArray.slice(0, idArray.length/2)
-    let half = dataArray.length/2
+    function mergeStarid(chunk1: starData[], chunk2: starData[]){
+        let dataArray = [];
+        while (chunk1.length && chunk2.length) {
+            // Pick the smaller among the smallest element of left and right sub arrays
+            if (chunk1[0].id < chunk2[0].id) {
+                dataArray.push(chunk1.shift());
+            } else {
+                dataArray.push(chunk2.shift());
+            }
+
+        }
+        return[...dataArray, ...chunk1, ...chunk2]
+    }
+    let half = dataArray.length/2;
     if (dataArray.length < 2){
         return dataArray
     }
-
-    let dataArrayLeft = dataArray.slice(0, half)
-    let dataArrayRight = dataArray.slice(half, dataArray.length)
+    let dataArrayLeft = dataArray.slice(0, half);
+    let dataArrayRight = dataArray.slice(half, dataArray.length);
     return mergeStarid(mergeSortStar(dataArrayLeft), mergeSortStar(dataArrayRight))
+
 }
 
-function mergeStarid(chunk1: starData[], chunk2: starData[]){
-    let dataArray = []
-    while (chunk1.length && chunk2.length) {
-        // Pick the smaller among the smallest element of left and right sub arrays
-        if (chunk1[0].id < chunk2[0].id) {
-            dataArray.push(chunk1.shift())
-        } else {
-            dataArray.push(chunk2.shift())
-        }
-
-    }
-    return[...dataArray, ...chunk1, ...chunk2]
-}
 
 function maxMinRaDec(star: starData, minMax: number[]){
-    let maxRa = minMax[0]
-    let minRa = minMax[1]
-    let maxDec = minMax[2]
-    let minDec = minMax[3]
+    let maxRa = minMax[0];
+    let minRa = minMax[1];
+    let maxDec = minMax[2];
+    let minDec = minMax[3];
     if (star.ra > maxRa){
-        maxRa = star.ra
+        maxRa = star.ra;
     }
     if (star.ra < minRa){
-        minRa = star.ra
+        minRa = star.ra;
     }
     if (star.dec > maxDec){
-        maxDec = star.dec
+        maxDec = star.dec;
     }
     if (star.dec < minDec){
-        minDec = star.dec
+        minDec = star.dec;
     }
     return [maxRa, minRa, maxDec, minDec]
 }
-//export function starDistnaceMotionArray()
+
+function getClusterCenter(minMax: number[], stars: any[]){
+    console.log(minMax)
+    let maxRa = minMax[0];
+    let minRa = minMax[1];
+    let maxDec = minMax[2];
+    let minDec = minMax[3];
+    let deltaRa = maxRa - minRa;
+    let wrap = false;
+    if (deltaRa > 180) {
+        wrap = true;
+        for (let star of stars) {
+            if (star['ra'] < minRa && star['ra'] > 180)
+                minRa = star['ra'];
+            if (star['ra'] > maxRa && star['ra'] < 180)
+                maxRa = star['ra'];
+        }
+        if (minRa - maxRa < 180)
+            return [0, 90, 90 - Math.abs(minDec)]
+    }
+    let centerRa = (minRa + maxRa)/2;
+    if (minRa - maxRa > 180)
+        if (centerRa > 180)
+            centerRa -= 180
+        else
+            centerRa += 180
+    let centerDec = (maxDec + minDec)/2
+    let radius = haversine(maxDec, centerDec, maxRa, centerRa)
+    return {'ra': centerRa, 'dec': centerDec, 'r': radius, 'wrap': wrap}
+}
+
+function haversine(dec1: number, dec2: number, ra1: number, ra2: number): number{
+    let dec1_rad = radians(dec1)
+    let dec2_rad = radians(dec2)
+    let ra1_rad = radians(ra1)
+    let ra2_rad = radians(ra2)
+    let theta = 2 * Math.asin((Math.sin((dec1_rad - dec2_rad)/2)**2 + Math.cos(dec1_rad) * Math.cos(dec2_rad) * (Math.sin((ra1_rad - ra2_rad)/2))**2) ** 0.5)
+    return (theta / Math.PI) * 180
+}
+
+function radians(degree: number): number{
+    return (degree/180 * Math.PI)
+}
