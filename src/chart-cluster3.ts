46f58d083a333816ebd69329579d1631ad23cdd4
Reed Fu <reed.fu@unc.edu>
2022-05-16 12:53:50 -0400

    enable saveData button in cluster pro ts file

--- src/chart-cluster3.ts
+++ src/chart-cluster3.ts
@@ -516,7 +516,7 @@ export function cluster3(): [Handsontable, Chart[], ModelForm, graphScale, Clust
     });
    }, 100);
 
-  document.getElementById('save-data-button').onclick = ()=>{clusterFileDownload(hot)}
+  document.getElementById('save-data-button').onclick = ()=>{clusterFileDownload(hot, [myChart1, myChart2], clusterForm, modelForm, [2, 2], graphMinMax, -1, clusterProForm)}
    //clusterProPmChartControl.oninput = throttle(function () {
     //clusterProButtonControl(myChart3);
   //}, 100);
